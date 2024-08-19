dde92642806a4662d7b10328660b302a65026ec1
Hubby <hubby@rmhubbert.com>
2024-08-19 09:53:50 +0100

    test: fix issue with tests accessing non existant fields

--- rmhttp_test.go
+++ rmhttp_test.go
@@ -18,10 +18,11 @@ var h HandlerFunc = HandlerFunc(func(w http.ResponseWriter, r *http.Request) err
 func Test_Handle(t *testing.T) {
 	app := New()
 	app.Handle("get", "/handle", h)
-	assert.Equal(t, 1, len(app.routes), "they should be equal")
+	routes := app.Routes()
+	assert.Equal(t, 1, len(routes), "they should be equal")
 
 	expectedKey := "GET /handle"
-	route, ok := app.routes[expectedKey]
+	route, ok := routes[expectedKey]
 	assert.Equal(t, true, ok, "they should be equal")
 	assert.Equal(t, "GET", route.Method(), "they should be equal")
 	assert.Equal(t, "/handle", route.Pattern(), "they should be equal")
@@ -32,10 +33,12 @@ func Test_Handle(t *testing.T) {
 func Test_HandleFunc(t *testing.T) {
 	app := New()
 	app.HandleFunc("get", "/handlefunc", h)
-	assert.Equal(t, 1, len(app.routes), "they should be equal")
+
+	routes := app.Routes()
+	assert.Equal(t, 1, len(routes), "they should be equal")
 
 	expectedKey := "GET /handlefunc"
-	route, ok := app.routes[expectedKey]
+	route, ok := routes[expectedKey]
 	assert.Equal(t, true, ok, "they should be equal")
 	assert.Equal(t, "GET", route.Method(), "they should be equal")
 	assert.Equal(t, "/handlefunc", route.Pattern(), "they should be equal")
