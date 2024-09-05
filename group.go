c0eb4ae46c97f0f9f4fc86ff4cbc2d4e122ae257
Hubby <hubby@rmhubbert.com>
2024-09-05 18:21:03 +0100

    feat: add method for adding subgroup

--- group.go
+++ group.go
@@ -34,3 +34,13 @@ func (group *Group) Route(route *Route) *Group {
 	group.Routes[route.String()] = route
 	return group
 }
+
+// Group adds the passed Group as a sub group to this Group.
+//
+// This method will return a pointer to the receiver Group, allowing the user to chain any of the
+// other builder methods that Group implements.
+func (group *Group) Group(g *Group) *Group {
+	group.Groups[group.Pattern] = g
+	g.Parent = group
+	return group
+}
