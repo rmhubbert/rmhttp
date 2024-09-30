35c81d0925a3ed0d2c69435134a8cfc0f097e644
Hubby <hubby@rmhubbert.com>
2024-09-30 13:02:08 +0100

    feat: add convenience function for creating simple handlers

--- handler.go
+++ handler.go
@@ -36,3 +36,15 @@ func (hf HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (hf HandlerFunc) ServeHTTPWithError(w http.ResponseWriter, r *http.Request) error {
 	return hf(w, r)
 }
+
+// createDefaultHandler creates and returns a HandlerFunc that simply sets the response status to
+// the passed code, and response body to the textual version of the same code.
+//
+// It is generally used to create default error handlers.
+func createDefaultHandler(code int) HandlerFunc {
+	return HandlerFunc(func(w http.ResponseWriter, r *http.Request) error {
+		w.WriteHeader(code)
+		_, _ = w.Write([]byte(http.StatusText(code)))
+		return nil
+	})
+}
