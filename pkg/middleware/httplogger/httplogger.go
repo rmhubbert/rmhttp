fbb3efd1e15acd7ab7f6618e6c8441f495d2816b
Hubby <hubby@rmhubbert.com>
2025-09-17 14:01:59 +0100

    feat: refactor to use httpsnoop & add IP address and user agent logging

--- pkg/middleware/httplogger/httplogger.go
+++ pkg/middleware/httplogger/httplogger.go
@@ -5,20 +5,20 @@ import (
 	"log/slog"
 	"net/http"
 	"strings"
-	"time"
 
-	"github.com/rmhubbert/rmhttp/pkg/capturewriter"
+	"github.com/felixge/httpsnoop"
 )
 
 func Middleware(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cw := capturewriter.New(w)
+			// NOTE: CaptureMetrics triggers next.ServeHTTP(w, r) for you, so do not run it manually as well.
+			m := httpsnoop.CaptureMetrics(next, w, r)
+			duration := fmt.Sprintf("%dms", m.Duration.Milliseconds())
+			code := m.Code
+			written := m.Written
 
-			start := time.Now()
-			next.ServeHTTP(cw, r)
-			duration := fmt.Sprintf("%vms", time.Since(start).Milliseconds())
-
+			ip := realIp(r)
 			host := strings.Join(r.Header.Values("X-Forwarded-For"), ",")
 			if host == "" {
 				host = r.Host
@@ -30,35 +30,62 @@ func Middleware(logger *slog.Logger) func(http.Handler) http.Handler {
 			if query != "" {
 				path += "?" + query
 			}
+			agent := r.UserAgent()
 
-			code := cw.Code
-			message := cw.Body
-
-			var hasErrorStatus = false
 			if code >= http.StatusBadRequest {
-				hasErrorStatus = true
-			}
-
-			if hasErrorStatus {
 				logger.Error(
-					message,
+					http.StatusText(code),
+					"status", code,
+					"ip", ip,
 					"method", r.Method,
 					"host", host,
 					"path", path,
-					"status", code,
+					"ua", agent,
+					"size", written,
 					"duration", duration,
 				)
 				return
 			}
 
 			logger.Info(
-				http.StatusText(code),
+				http.StatusText(m.Code),
+				"status", code,
+				"ip", ip,
 				"method", r.Method,
 				"host", host,
 				"path", path,
-				"status", code,
+				"ua", agent,
+				"size", written,
 				"duration", duration,
 			)
 		})
 	}
+}
+
+// Request.RemoteAddress contains the port, which is not desired.
+func removePort(ra string) string {
+	index := strings.LastIndex(ra, ":")
+	if index == -1 {
+		return ra
+	}
+	return ra[:index]
+}
+
+// requestGetRemoteAddress returns ip address of the client making the request,
+// taking into account http proxies
+func realIp(r *http.Request) string {
+	header := r.Header
+	xRealIP := header.Get("X-Real-Ip")
+	xForwardedFor := header.Get("X-Forwarded-For")
+	if xRealIP == "" && xForwardedFor == "" {
+		return removePort(r.RemoteAddr)
+	}
+	if xForwardedFor != "" {
+		parts := strings.Split(xForwardedFor, ",")
+		for i, p := range parts {
+			parts[i] = strings.TrimSpace(p)
+		}
+		return parts[0]
+	}
+	return xRealIP
 }
